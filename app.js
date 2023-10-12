19383c1470fdfa2f610cdc144aeff6f424820638
manpreetsingh04m <ms2404mk@gmail.com>
2023-10-12 17:01:53 +0530

    Complete

---
+++ app.js
@@ -0,0 +1,60 @@
+const toggle = document.getElementById('toggleDark');
+const body = document.querySelector('body');
+const link1 = document.getElementById('mail');
+const link2 = document.getElementById('skill');
+const link3 = document.getElementById('about');
+const link4 = document.getElementById('project');
+const link5 = document.getElementById('hr1');
+const link6 = document.getElementById('hr2');
+const link7 = document.getElementById('hr3');
+const link8 = document.getElementById('hr4');
+const link9 = document.getElementById('hr5');
+
+toggle.addEventListener('click', function(){
+    this.classList.toggle('bi-moon');
+    if(this.classList.toggle('bi-brightness-high-fill')){
+        body.style.background = 'white';
+        body.style.color = 'rgb(17,16,16)';
+        link1.style.color = 'rgb(17,16,16)';
+        link2.style.color = 'rgb(17,16,16)';
+        link3.style.color = 'rgb(17,16,16)';
+        link4.style.color = 'rgb(17,16,16)';
+        link5.style.color = 'rgb(17,16,16)';
+        link6.style.color = 'rgb(17,16,16)';
+        link7.style.color = 'rgb(17,16,16)';
+        link8.style.color = 'rgb(17,16,16)';
+        link9.style.color = 'rgb(17,16,16)';
+        body.style.transition = '0.8s';
+        link1.style.transition = '0.8s';
+        link2.style.transition = '0.8s';
+        link3.style.transition = '0.8s';
+        link4.style.transition = '0.8s';
+        link5.style.transition = '0.8s';
+        link6.style.transition = '0.8s';
+        link7.style.transition = '0.8s';
+        link8.style.transition = '0.8s';
+        link9.style.transition = '0.8s';
+    }else{
+        body.style.background = 'rgb(17,16,16)';
+        body.style.color = 'white';
+        link1.style.color = 'white';
+        link2.style.color = 'white';
+        link3.style.color = 'white';
+        link4.style.color = 'white';
+        link5.style.color = 'white';
+        link6.style.color = 'white';
+        link7.style.color = 'white';
+        link8.style.color = 'white';
+        link9.style.color = 'white';
+        body.style.transition = '0.8s';
+        link1.style.transition = '0.8s';
+        link2.style.transition = '0.8s';
+        link3.style.transition = '0.8s';
+        link4.style.transition = '0.8s';
+        link5.style.transition = '0.8s';
+        link6.style.transition = '0.8s';
+        link7.style.transition = '0.8s';
+        link8.style.transition = '0.8s';
+        link9.style.transition = '0.8s';
+    }
+});
